706d494330913bdc82e05cc98103fbd0a178b3ba
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-08 16:24:14 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+from os import environ
+
+from discord import __version__ as discver, Activity, ActivityType
+from discord.ext.commands import Bot
+
+from helpers.helpers import prefix
+
+bot = Bot(
+	activity=Activity(
+		name='.help | D&D 5e',
+		type=ActivityType.watching
+	),
+	command_prefix=prefix,
+	pm_help=True
+)
+
+
+@bot.event
+async def on_ready():
+	print(f"I'm ready.\n{bot.user}\n{discver}")
+
+
+bot.load_extension('cogs.rollingcog')
+bot.load_extension('cogs.taverncog')
+
+bot.run(environ.get('DISCORD_BOT_SECRET'))
