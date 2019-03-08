79e21936fe3749a04895c681e1e674317dcb23c9
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-08 16:26:27 +0100

    Update checks.py

--- helpers/checks.py
+++ helpers/checks.py
@@ -1,1 +1,9 @@
+from discord.ext.commands import check
 
+
+def is_tavern():
+    def predicate(ctx):
+        if not ctx.guild.id in (362589385117401088, 546007130902233088):
+            return False
+        return True
+    return check(predicate)
