6617281b6ca095eb689f36547e5bde0fa7abfade
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-12 17:32:38 +0100

    Made it so it actually iterated trough commands

--- cogs/generatorcog.py
+++ cogs/generatorcog.py
@@ -18,13 +18,14 @@ class GeneratorCog(Cog, name='Generator'):
         """All of the generate commands that are used to generate things, such as:
         characters, npc's and names."""
         generator_embed = Embed(colour=Colour.blurple())
-        commands = ['bond', 'flaw']
+        commands = ['Bond', 'Flaw']
         desc = ''
         num = 0
         if generate_num is None:
             generator_embed.title = 'All of the Generator Commands'
             for _ in commands:
-                desc += f'***{commands[num]}*** \n'
+                desc += f'**{commands[num]}** \n'
+                num = num + 1
             generator_embed.description = desc
             generator_embed.set_footer(text='Use ;generate {command} to use one of the above commands.')
             return await ctx.send(embed=generator_embed)
