763047964e4a546ba1d734af962aabce448066f5
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-12 21:32:34 +0100

    Small fixes due to an error when trying to merge.

--- cogs/generatorcog.py
+++ cogs/generatorcog.py
@@ -16,21 +16,22 @@ class GeneratorCog(Cog, name='Generator'):
         self.bot = bot
 
     @command(name='generate')
-    async def generator_command(self, ctx, generate_num):
+    async def generator_command(self, ctx, generate_num=None):
         """All of the generate commands that are used to generate things, such as:
         characters, npc's and names."""
         generator_embed = Embed(colour=Colour.blurple())
         commands = ['bond', 'flaw']
         desc = ''
         num = 0
-        if not generate_num:
+        if generate_num is None:
             generator_embed.title = 'All of the Generator Commands'
             for _ in commands:
                 desc += f'***{commands[num]}*** \n'
             generator_embed.description = desc
             generator_embed.set_footer(text='Use ;generate {command} to use one of the above commands.')
             return await ctx.send(embed=generator_embed)
-        with open(paths[generate_num], 'r') as f:
+        final = str.casefold(generate_num)
+        with open(paths[final], 'r') as f:
             strings = f.readlines()
         return await ctx.send(random.choice(strings))
 
