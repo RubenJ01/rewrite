ea08011964aa16d243682760203ac00b6741c168
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-14 10:43:37 +0100

    Added quests and lowered the cap on generating

--- cogs/generatorcog.py
+++ cogs/generatorcog.py
@@ -13,6 +13,7 @@ paths = {
     "ideal": Path('resources') / 'ideals.txt',  # list of ideals
     "trait": Path('resources') / 'traits.txt',  # list of traits
     "townname": Path('resources') / 'townnames.txt',  # list of town names
+    "quest": Path('resources') / 'quests.txt',  # list of quests
 }
 
 
@@ -27,7 +28,7 @@ class GeneratorCog(Cog, name='Generator'):
         characters, NPCs and names."""
         log.debug(f'generate request with type={generate} and amount={amount}')
         generator_embed = Embed(colour=Colour.blurple())
-        commands = ['bond', 'flaw', 'ideal', 'townname', 'trait']
+        commands = ['bond', 'flaw', 'ideal', 'quest', 'townname', 'trait']
         desc = ''
         num = 0
         if generate not in commands:  # user requested an invalid option; show help
@@ -49,8 +50,8 @@ class GeneratorCog(Cog, name='Generator'):
         except ValueError:
             return await ctx.send(f'{amount} is not a valid number of {final}s to generate.')
         else:
-            if not 1 < iamount <= 10:
-                return await ctx.send(f'Please choose a number of {final}s between 2 and 10.')
+            if not 1 < iamount <= 5:
+                return await ctx.send(f'Please choose a number of {final}s between 2 and 5.')
             return await ctx.send(''.join(random.choices(strings, k=iamount)))
 
 
