b3eedb2791e066eb23001854cb2cbb7ef4dc1dd5
syntaxaire <syntaxaire@gmail.com>
2019-03-26 20:56:38 -0400

    Rework SRD access into a class with search capability

--- backends/srd.py
+++ backends/srd.py
@@ -2,18 +2,78 @@
 
 import json
 import logging
+from functools import lru_cache
 from pathlib import Path
+from time import perf_counter_ns
 
 log = logging.getLogger('bot.' + __name__)
 
 SRDPATH = Path('resources') / 'srd'
 
-srd = {}  # import this name for access to the SRD
 
-# map SRD resource type to raw JSON data, e.g. 'spells' to contents of 'resources/srd/5e-SRD-Spells.json'
-for file in SRDPATH.iterdir():
-    if file.name.endswith('.json'):
-        resource = file.stem.replace('5e-SRD-', '').lower()
-        log.debug(f'Loading SRD: {resource} from {file}')
-        with open(file, encoding='utf-8') as handle:
-            srd[resource] = json.load(handle)
+def collapse(item) -> str:
+    """Given a JSON-derived data structure, collapse all found strings into one."""
+    result = ''
+    if isinstance(item, str):
+        return item
+    elif isinstance(item, list):
+        for subitem in item:
+            result += collapse(subitem)
+        return result
+    elif isinstance(item, dict):
+        for subitem in item.values():
+            result += collapse(subitem)
+        return result
+    else:
+        return ''
+
+
+class __SRD:
+    """Contains the imported SRD data and methods to search it."""
+    def __init__(self, data_path: Path):
+        self.raw = {}  # will contain data from all JSON files
+        # map SRD resource type to raw JSON data, e.g. 'spells' to contents of 'resources/srd/5e-SRD-Spells.json'
+        for file in data_path.iterdir():
+            if file.name.endswith('.json'):
+                resource = file.stem.replace('5e-SRD-', '').lower()
+                log.debug(f'Loading SRD: {resource} from {file}')
+                with open(file, encoding='utf-8') as handle:
+                    self.raw[resource] = json.load(handle)
+
+    @lru_cache(maxsize=1024)
+    def search(self, resource: str, attr: str, request: str) -> (list, bool):
+        """Do a text search of one attribute of one SRD resource.
+        Returns a list of results (empty if none) and a flag indicating whether or not the search
+        was truncated.
+        Example:
+            search('spells', 'name', 'missile')
+        will search the spells resource for a spell with 'missile' in the 'name' attribute.
+        Repeated identical searches will be cached by decorator."""
+        log.debug(f'Searching \'{resource}\'->\'{attr}\' for \'{request}\'...')
+        start_time = perf_counter_ns()
+        # check if the request makes sense
+        try:
+            target = self.raw[resource]
+        except ValueError:
+            log.debug(f'Invalid search: resource \'{resource}\' not found.')
+            return []
+        if attr not in target[0]:
+            log.debug(f'Invalid search: \'{resource}\' does not have attribute \'{attr}\'')
+            return []
+        # do the search
+        results, truncated = [], False
+        for item in target:
+            search_text = collapse(item[attr]).lower()
+            if request in search_text.lower():
+                results.append(item)
+                # stop matchy searches before they get too long
+                if len(results) > 5:
+                    truncated = True
+                    break
+        end_time = perf_counter_ns()
+        elapsed_ms = (end_time - start_time) / 1_000_000
+        log.debug(f'Finished search in {elapsed_ms}ms')
+        return results, truncated
+
+
+srd = __SRD(SRDPATH)  # for export: for access to the SRD
--- cogs/infocog.py
+++ cogs/infocog.py
@@ -1,6 +1,6 @@
 import logging
 
-from discord import Colour, Embed
+# from discord import Colour, Embed
 from discord.ext.commands import Cog, command
 
 from backends.srd import srd
@@ -14,24 +14,19 @@ class InfoCog(Cog, name='Information'):
 
     @command(name='spell')
     async def spell_command(self, ctx, *request):
-        spells = srd['spells']
         request = ' '.join(request).lower()
         log.debug(f'spell command called with request: {request}')
-        if len(request) <= 3:
+        if len(request) <= 2:
             return await ctx.send('Request too short.')
-        matches = []
-        for spell in spells:
-            if request in spell['name'].lower():
-                matches.append(spell)
+        matches, truncated = srd.search('spells', 'name', request)
         if len(matches) == 0:
             return await ctx.send(f'Couldn\'t find any spells that match \'{request}\'.')
         if len(matches) > 1:
             spell_names = [match['name'] for match in matches]
             return await ctx.send(f'Could be any of {spell_names}.')
-        else:
-            match = matches[0]
-            spell_name = match['name']
-            return await ctx.send(f'The spell you wanted is {spell_name}.')
+        match = matches[0]
+        spell_name = match['name']
+        return await ctx.send(f'The spell you wanted is {spell_name}.')
 
 
 def setup(bot):
