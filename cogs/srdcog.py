5748c23b644d125ae7f561ba6a0b5240f2bec0d8
syntaxaire <syntaxaire@gmail.com>
2019-03-27 22:15:32 -0400

    srdcog: create rich embeds based on extracted spell info

--- cogs/srdcog.py
+++ cogs/srdcog.py
@@ -1,11 +1,13 @@
 import logging
 
-# from discord import Colour, Embed
+from discord import Colour, Embed
 from discord.ext.commands import Cog, command
 
-from backends.srd import srd
+from backends.srd_json import srd, get_spell_info
 
 log = logging.getLogger('bot.' + __name__)
+
+PHB_COLOUR = Colour(0xeeeea0)
 
 
 class SRDCog(Cog, name='SRD Information'):
@@ -14,19 +16,29 @@ class SRDCog(Cog, name='SRD Information'):
 
     @command(name='spell')
     async def spell_command(self, ctx, *request):
+        # TODO: Handle spells that exceed Discord embed size limits, like Imprisonment
+        """Give information on a spell by name."""
         request = ' '.join(request).lower()
         log.debug(f'spell command called with request: {request}')
         if len(request) <= 2:
             return await ctx.send('Request too short.')
-        matches, truncated = srd.search('spells', 'name', request)
+        matches, truncated = srd.search('spells', 'name', request, trunc=0)
         if len(matches) == 0:
             return await ctx.send(f'Couldn\'t find any spells that match \'{request}\'.')
         if len(matches) > 1:
             spell_names = [match['name'] for match in matches]
-            return await ctx.send(f'Could be any of {spell_names}.')
-        match = matches[0]
-        spell_name = match['name']
-        return await ctx.send(f'The spell you wanted is {spell_name}.')
+            return await ctx.send(f'Could be: {", ".join(spell_names)}.')
+        spell = matches[0]
+        info = get_spell_info(spell)
+        embed = Embed(title=info.name,
+                      colour=PHB_COLOUR,
+                      description=f'*{info.subhead}*\n{info.description}')
+        embed.add_field(name="Casting Time", value=info.casting_time, inline=True)
+        embed.add_field(name="Range", value=info.casting_range, inline=True)
+        embed.add_field(name="Components", value=info.components, inline=True)
+        embed.add_field(name="Duration", value=info.duration, inline=True)
+        embed.set_footer(text=f'Player\'s Handbook, page {info.page}.')
+        return await ctx.send(embed=embed)
 
 
 def setup(bot):
