a747e0b4aa07d6f69b75ac80015f0d2f2e111eb1
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-09-05 17:36:58 +0200

    new help cmd can now display details about specific cmds

--- cogs/specialcog.py
+++ cogs/specialcog.py
@@ -127,24 +127,56 @@ class SpecialCog(Cog, name='Special'):
             return await ctx.send(f'Could not change the prefix for the server.\n{error}')
 
     @command(name='help')
-    async def help_(self, ctx):
+    async def help_(self, ctx, second_help: str = None):
         cogs = sorted([cog for cog in self.bot.cogs.keys() if cog not in ['ErrorHandler', 'Tavern']])
         pages = []
         page = 1
-        for cog_name in cogs:
-            cog = self.bot.get_cog(cog_name)
-            commands = [cmd for cmd in cog.get_commands() if not cmd.hidden or cmd.name == 'help']
-            message = cog.description + '\n'
-            for cmd in commands:
-                message += f' \n  **{self.config["prefix"]}{cmd}** \n *{cmd.help}*'
-            help_embed = Embed(title=cog_name, colour=Colour.blurple(), description=message)
-            help_embed.set_footer(text=f'Page: {page}/{len(cogs)}')
-            help_embed.set_author(name=f'{ctx.author}', icon_url=ctx.author.avatar_url)
-            pages.append(help_embed)
-            page = page + 1
-        embed = Paginator(embed=False, timeout=90, use_defaults=True,
-                          extra_pages=pages, length=1)
-        await embed.start(ctx)
+        cmd_names = [cmd.name for cmd in self.bot.commands]
+        if not second_help:
+            for cog_name in cogs:
+                cog = self.bot.get_cog(cog_name)
+                commands = [cmd for cmd in cog.get_commands() if not cmd.hidden or cmd.name == 'help']
+                message = cog.description + '\n'
+                for cmd in commands:
+                    message += f' \n  **{self.config["prefix"]}{cmd}** \n *{cmd.help}*'
+                help_embed = Embed(title=cog_name, colour=Colour.blurple(), description=message)
+                help_embed.set_footer(text=f'Page: {page}/{len(cogs)}')
+                help_embed.set_author(name=f'{ctx.author}', icon_url=ctx.author.avatar_url)
+                pages.append(help_embed)
+                page = page + 1
+            embed = Paginator(embed=False, timeout=90, use_defaults=True,
+                              extra_pages=pages, length=1)
+            await embed.start(ctx)
+        else:
+            if second_help.lower() in cmd_names:
+                cmd = self.bot.get_command(second_help)
+                embed = Embed(title=cmd.name, colour=Colour.blurple())
+                value = ''
+                if cmd.aliases:
+                    for alias in cmd.aliases:
+                        value += f'{str(alias)}, '
+                    value = value[0:-2]
+                    value = value + '.'
+                else:
+                    value = None
+                embed.add_field(name="Aliases", value=f'*{value}*', inline=False)
+                params_list = list(cmd.params.keys())
+                req_params = []
+                for value in params_list:
+                    req_params.append(value)
+                req_params.remove('self')
+                req_params.remove('ctx')
+                param_message = 'Required parameters are:\n**'
+                if req_params:
+                    for parm in req_params:
+                        param_message += parm + '\n'
+                    embed.add_field(name='Usage', value=param_message + '**', inline=False)
+                else:
+                    embed.add_field(name='Usage', value=param_message + 'None**', inline=False)
+                embed.set_author(name=f'{ctx.author}', icon_url=ctx.author.avatar_url)
+                return await ctx.send(embed=embed)
+            else:
+                return await ctx.send(f"{str(second_help)} command does not exist!")
 
     @is_admin()
     @command(name='hiddencmds', aliases=['hiddens'], hidden=True)
