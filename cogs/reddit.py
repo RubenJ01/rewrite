dba40ce3cbdbcfbc03ed31306d9fb22f6bac17e3
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-09-07 12:06:51 +0200

    merged subreddits and reddit cog together

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,12 +1,16 @@
 import asyncio
 import logging
 import random
+import discord
+
 from collections import deque
 
-import discord
 from discord.ext import commands
 
-from utils.database.db_functions import guild_subreddits
+from sqlalchemy import update
+from utils.database.db_functions import guild_subreddits, db_edit, db_query, cache_subreddits
+import utils.database as tables
+
 
 log = logging.getLogger('bot.' + __name__)
 
@@ -18,6 +22,7 @@ class DndReddit(commands.Cog, name='D&D Reddit'):
         self.subreddits = bot.config['reddit']['subreddits']
         self.img_cache = deque(maxlen=10)
         self.cache_clear_task = bot.loop.create_task(self.clear_cache())
+        self.default_subreddits = bot.config['reddit']['subreddits']
 
     async def clear_cache(self):
         self.img_cache.clear()
@@ -33,6 +38,115 @@ class DndReddit(commands.Cog, name='D&D Reddit'):
 
         async with session.get(url=url, params=params, headers=headers) as response:
             return await response.json()
+
+    @commands.group(name="subreddit", invoke_without_command=True)
+    async def subreddit(self, ctx):
+        pass
+
+    @subreddit.command(name="enable")
+    async def enable_feature(self, ctx):
+        """A command to enable specific subreddits feature."""
+
+        code = tables.subreddits.insert().values()
+        data = {
+            'guild_id': ctx.guild.id,
+            'subreddit_names': ""
+        }
+        result = await db_edit(code, data)
+        if result:
+            await ctx.send("Feature has been enabled!")
+        else:
+            await ctx.send("Feature has already been enabled!")
+
+    @subreddit.command(name="add")
+    async def add_subreddit(self, ctx, subreddit: str):
+        """Add a subreddit so users in the guild can get posts from it."""
+        table = tables.subreddits
+        row_data = await db_query(table.select().where(table.c.guild_id == ctx.guild.id))
+        try:
+            subreddits_string = row_data[0][1]
+        except IndexError:
+            return await ctx.send("This feature is not enabled!")
+
+        if subreddits_string == "":
+            new_subreddits = subreddits_string + subreddit + ","
+
+        else:
+            subreddit_list = subreddits_string.split(',')
+            if subreddit in subreddit_list:
+                return await ctx.send(f"Subreddit **{subreddit}** is already present!")
+            new_subreddits = subreddits_string + subreddit + ","
+
+        data = {
+            'subreddit_names': new_subreddits
+        }
+
+        result = await db_edit(update(table).where(table.c.guild_id == ctx.guild.id).values(), data)
+        if result:
+            await cache_subreddits()
+            await ctx.send(f"Subreddit **{subreddit}** has been added!")
+        else:
+            await ctx.send("fail")
+
+    @subreddit.command(name="remove")
+    async def remove_subreddit(self, ctx, subreddit: str):
+        """Remove a subreddit from the list of accessible subreddits on the server."""
+        table = tables.subreddits
+        row_data = await db_query(table.select().where(table.c.guild_id == ctx.guild.id))
+        try:
+            subreddits_string = row_data[0][1]
+        except IndexError:
+            return await ctx.send("This feature is not enabled!")
+
+        if subreddits_string == "":
+            return await ctx.send("Nothing to remove, list is empty.")
+
+        else:
+            subreddit_list = subreddits_string.split(',')
+            if subreddit not in subreddit_list:
+                return await ctx.send(f"Cannot find subreddit **{subreddit}** in the list!")
+            subreddit_list.remove(subreddit)
+            new_subreddits = ",".join(subreddit_list)
+
+        data = {
+            'subreddit_names': new_subreddits
+        }
+
+        result = await db_edit(update(table).where(table.c.guild_id == ctx.guild.id).values(), data)
+        if result:
+            await cache_subreddits()
+            await ctx.send(f"Subreddit **{subreddit}** has been removed!")
+        else:
+            await ctx.send("fail")
+
+    @subreddit.command(name="view")
+    async def view_subreddits(self, ctx):
+        """View all avilable subreddits."""
+        table = tables.subreddits
+        row_data = await db_query(table.select().where(table.c.guild_id == ctx.guild.id))
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.title = "Accessable Subreddits"
+
+        try:
+            subreddits_string = row_data[0][1]
+        except IndexError:
+            return await ctx.send("This feature is not enabled!")
+
+        if subreddits_string == "":
+            embed.description = "There are no added subreddits."
+
+        else:
+            subreddit_list = subreddits_string.split(",")
+            subreddit_list.pop()
+
+            embed.description = ""
+            for i, sr in enumerate(subreddit_list):
+                embed.description += f"{i+1}.{sr}\n"
+
+        embed.description += "\n**Default subreddits are:**\n"
+        for sr in self.default_subreddits:
+            embed.description += f"{sr}\n"
+        await ctx.send(embed=embed)
 
     @commands.command(name='reddit')
     async def get_reddit(self, ctx, subreddit='dndmemes'):
